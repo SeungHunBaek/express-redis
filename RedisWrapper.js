0ff3d60e14db35f3f734bfd5bc69d6f7cdd9525d
baek <xemeal@gmail.com>
2021-01-22 22:11:50 +0900

    cache만료시간 지정기능 추가

--- RedisWrapper.js
+++ RedisWrapper.js
@@ -10,20 +10,36 @@ client.on("ready", () => {
 
 let Store = class {
 
-    set(slackId, token){
-        console.log("[redis]set");
+    set(slackId, token, expireTime){
+        console.log("[redis.expire]start");
+        console.log("[redis.set]slackId :" + slackId+", token: "+token);
         client.set(slackId, token);
+        if(expireTime) {
+            this.expire(slackId, expireTime);
+        }
+
     };
 
     get(slackId){
         return new Promise((resolve, reject) => {
             client.get(slackId, (err, result) => {
+                if(err){
+                    reject(resolve);
+                }
+                console.log("[redis.get]start");
+                console.log("[redis.get]slackId :" + slackId+", token: "+result);
                 resolve(result);
             });
         });
 
     };
+    
+    expire(slackId, expireTime) {
+        console.log("[redis.expire]start");
+        console.log("[redis.expire]slackId :" + slackId+", expireTime: "+expireTime);
+        client.expire(slackId, expireTime);
+    }
 }
 
 // module.exports = Store;
-export default Store;
+export default new Store();
--- app.js
+++ app.js
@@ -11,18 +11,21 @@ app.get("/set/:id/:token", (req, res) => {
     const token = req.params.token;
     console.log(slackId+token);
     redisInstance.set(slackId, token);
-    
     res.sendStatus(200);
-
 });
+app.get("/set/:id/:token/:expire", (req, res) => {
 
-
+    const slackId = req.params.id;
+    const token = req.params.token;
+    const expire = req.params.expire;
+    redisInstance.set(slackId, token, expire);
+    res.sendStatus(200);
+});
 app.get("/get/:id/", async (req, res) => {
 
     const slackId = req.params.id;
-    
     const token = await redisInstance.get(slackId);
-    res.send(token);
+    res.send("slackId: "+slackId+", token: "+token);
 });
 
 app.listen(3003, () => {
