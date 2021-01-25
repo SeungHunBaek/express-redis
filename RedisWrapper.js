11925ba37075d11004f20f7b7610ec25641d7def
baek <xemeal@gmail.com>
2021-01-25 22:05:32 +0900

    Cache만료시간 지정 기능 추가

--- RedisWrapper.js
+++ RedisWrapper.js
@@ -11,34 +11,47 @@ client.on("ready", () => {
 let Store = class {
 
     set(slackId, token, expireTime){
-        console.log("[redis.expire]start");
-        console.log("[redis.set]slackId :" + slackId+", token: "+token);
-        client.set(slackId, token);
-        if(expireTime) {
-            this.expire(slackId, expireTime);
+        try {
+            console.log("[redis.expire]start");
+            console.log("[redis.set]slackId :" + slackId+", token: "+token);
+            client.set(slackId, token);
+            if(expireTime) {
+                this.expire(slackId, expireTime);
+            }    
+        } catch (error) {
+            throw(error);
         }
+        
 
     };
 
     get(slackId){
         return new Promise((resolve, reject) => {
-            client.get(slackId, (err, result) => {
-                if(err){
-                    reject(resolve);
-                }
-                console.log("[redis.get]start");
-                console.log("[redis.get]slackId :" + slackId+", token: "+result);
-                resolve(result);
-            });
+            try {
+                client.get(slackId, (err, result) => {
+                    if(err){
+                        reject(resolve);
+                    }
+                    console.log("[redis.get]start");
+                    console.log("[redis.get]slackId :" + slackId+", token: "+result);
+                    resolve(result);
+                });
+            } catch (error) {
+                throw(error)
+            }
         });
-
     };
     
     expire(slackId, expireTime) {
-        console.log("[redis.expire]start");
-        console.log("[redis.expire]slackId :" + slackId+", expireTime: "+expireTime);
-        client.expire(slackId, expireTime);
+        try {
+            console.log("[redis.expire]start");
+            console.log("[redis.expire]slackId :" + slackId+", expireTime: "+expireTime);
+            client.expire(slackId, expireTime);
+        } catch (error) {
+            throw(error)
+        }
     }
+
 }
 
 // module.exports = Store;
